490f97fa92779fb68ef5e83436cc49be8e220cf1
Woff12 <153305538+Woff12@users.noreply.github.com>
2024-10-31 19:58:59 +0300

    Create 2024.10.31.04.main.cpp

---
+++ 2024.10.31.04.main/2024.10.31.04.main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+class Money {
+private:
+    int dollars;
+    int cents;
+
+public:
+    
+    explicit Money(int d, int c) : dollars(d), cents(c) {}
+
+    void print() {
+        cout << dollars << " долларов " << cents << " центов" << endl;
+    }
+};
+
+int main() {
+    
+    Money amount1(10, 50);
+    amount1.print();
+
+    Money amount3 = static_cast<Money>(20); 
+    amount3.print();
+
+
+
+    return 0;
+}
