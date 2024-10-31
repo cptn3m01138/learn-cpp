ba12dd452725ff2379870a28496297e19225f543
Woff12 <153305538+Woff12@users.noreply.github.com>
2024-10-31 19:15:37 +0300

    Create 2024.10.31.03.main.cpp

---
+++ 2024.10.31.03.main/2024.10.31.03.main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+class Rectangle {
+private:
+    double width;
+    double height;
+
+public:
+    Rectangle(double w, double h) : width(w), height(h) {}
+
+    double getArea() {
+        return width * height;
+    }
+
+    void printInfo() {
+        cout << "Rectangle with width " << width << " and height " << height << endl;
+    }
+};
+
+int main() {
+    Rectangle rect(5.0, 3.0);
+
+    rect.printInfo();
+
+    cout << "acreage : " << rect.getArea() << endl;
+
+    return 0;
+}
